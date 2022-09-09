1574bc337c6a41046cf9258c61b5547aa0d16fb0
Juana <juanavalentinarico@gmail.com>
2022-09-09 15:01:41 -0500

    ejericio 9 manual

--- ejercicio/main.c
+++ ejercicio/main.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-    printf("argc: %d\n",argc);
+    printf("argc: %d\n", argc);
 
-    for(int i=0; i<5;i++)
+    for (int i = 0; i < argc; i++)
     {
-        printf("argc[%d]: %s\n",i, argv[i]);//%S es para direccion de memoria de caracteres (%p es para imprimir el contenido de un ountero)
+        printf("argc[%d]: %s\n", i, argv[i]); //%S es para direccion de memoria de caracteres (%p es para imprimir el contenido de un ountero)
     }
+
+    // tratando de hacerlo manualmente
+
+    
+
+    for (int i=0; i < argc; i++)
+    {
+        char *pfirst = argv[i];
+        char *pfirstChar = pfirst;
+
+        while (*pfirstChar != 0)
+        {
+            printf("%c", *pfirstChar);
+
+            pfirstChar = pfirstChar + 1; // para apuntar al siguiente caracter
+        }
+        printf("\n");
+    }
+
+ 
 
     return EXIT_SUCCESS;
 }
